1af0c1299ff4dc763318dd83163436b1e1c779cf
jtrout <jd.trout@loni.ucla.edu>
2010-09-16 15:56:56 -0700

    Sanitized app for github push.

--- test/connect.rb
+++
@@ -1,8 +0,0 @@
-   require 'rubygems'
-   require 'net/ssh'
-   
- 
- Net::SSH.start('ifsnl.loni.ucla.edu', 'root', :password => "#{}1fsn{!") do |ssh|
-   hostname = ssh.exec!("hostname")
-  puts hostname
- end
